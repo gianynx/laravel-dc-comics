0ba5507d41a313dc3af581f6e7865fd47cf8571a
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-29 16:36:44 +0200

    add migration

---
+++ database/migrations/2023_05_29_131749_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 200);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price')->default(0);
+            $table->string('series', 200);
+            $table->date('sale_date')->nullable();
+            $table->string('type', 200);
+            $table->json('artists');
+            $table->json('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
